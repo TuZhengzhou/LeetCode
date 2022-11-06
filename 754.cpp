5829d0b0b5a0593c01a99bfa002725291ec7c9e2
TuZhengzhou <2098853775@qq.com>
2022-11-06 13:30:40 +0800

    754 update

--- 754.cpp
+++ 754.cpp
@@ -13,11 +13,11 @@ public:
             sum += idx;
         }
         if ((sum - target) % 2)
-            return idx + 1 + idx % 2;
-        // if(idx % 2)
-        //     return idx+2;
-        // else
-        //     return idx+1;
+            // return idx + 1 + idx%2;
+            if ((idx + 1) % 2)
+                return idx + 1;
+            else
+                return idx + 2;
         else
             return idx;
     }
